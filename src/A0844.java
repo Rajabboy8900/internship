b49224a73df2343e4bfa529a426fa6260165460f
rajabboy <rajabbboyrajabov@gmail.com>
2025-08-02 10:48:40 +0500

    added844

---
+++ src/A0844.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class A0844 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        long a = cin.nextLong();
+        long b = cin.nextLong();
+
+        double c = Math.sqrt(a * b);
+        if (c % 1 == 0) {
+            System.out.print((int) c);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
