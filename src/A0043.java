d082f3319b3d89c8fc633fc4ce600eff240720a7
rajabboy <rajabbboyrajabov@gmail.com>
2025-08-02 10:12:24 +0500

    added 0043

--- src/A0043.java
+++ src/A0043.java
@@ -1,3 +1,29 @@
-public class A0043 {
+import java.util.Scanner;
 
+public class A0043 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        String n = cin.next();
+        int max = 0;
+        int count = 0;
+
+        for (int i = 0; i < n.length(); i++) {
+            if (n.charAt(i) == '0') {
+                count++;
+                if (count > max) {
+                    max = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+
+
+
+
+
+
+        System.out.println(max);
+    }
 }
